53f5fec12d7bab6192619ec8b52ada9e4cd5f4d5
PRO-GUNE <chathuragunasekera@gmail.com>
2022-03-28 12:12:29 +0530

    96% Working Bot v2.0

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ This is a wordle bot designed using the principles of entropy in information the
 
 ## Bot v1.0 - Naive bot
 This bot takes the probability of choosing each letter for a given position of a word to be equally likely
+
+## Bot v2.0 - FrequencyData Bot
+This bot uses data about the frequency of usage of words to predict the probable words
+
+### Calculations
+* Expected Value / Entropy : Calculate the matching words for a given arrangement and then use the frequecy data of those words to calculate the overall probability of that arrangement. Use this probability to calculate the Entropy value
+* Score: The score is calculated using the following function
+*Score = P(word)x(number_of_moves) + (1-P(word))x(F(current_uncertainity-expected_value_of_word))*
+The Function *F* is a function that gives how likely it is to move to the next guess without the current guess being the correct guess
+
+### Results
+When checked upon the valid wordle dataset, this bot can execute with 96% success rate and mean score of 4.21
--- simulator.py
+++ simulator.py
@@ -34,28 +34,28 @@ def genFeedback(word, guess):
         print(f"Some Type Erro Caught word:{word}")
 
 def printMoves(moves, stdscr, screen_row_offset):
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_YELLOW)
-    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_YELLOW)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
     GREEN = curses.color_pair(1)
     YELLOW = curses.color_pair(2)
-    BLACK = curses.color_pair(3)
+    WHITE = curses.color_pair(3)
 
     for i,move in enumerate(moves, screen_row_offset):
         j=0
         for letter in move:
             if letter=="R":
-                stdscr.addstr(i,j,"  ",GREEN)
+                stdscr.addstr(i,j,"  ",GREEN | curses.A_DIM)
             elif letter=="Y":
-                stdscr.addstr(i,j,"  ",YELLOW)
+                stdscr.addstr(i,j,"  ",YELLOW | curses.A_DIM)
             elif letter=="G":
-                stdscr.addstr(i,j,"  ",BLACK)
+                stdscr.addstr(i,j,"  ",WHITE | curses.A_DIM)
             j+=2
 
 
 
 
-def simulate(stdscr:str, all_filename:str, allowed_filename:str, initial_guess:str="tares")->list:
+def simulate(stdscr:str, all_filename:str, allowed_filename:str, initial_guess:str=None)->list:
     '''
         Simulate a number of tests with the given wordle bot
     '''
@@ -65,11 +65,11 @@ def simulate(stdscr:str, all_filename:str, allowed_filename:str, initial_guess:s
     all_guesses = []
     # All the arrangements
     arramgements = product("RYG", repeat=5)
+    # Make a copy of the raw word list
+    words = readFile(all_filename)
 
     # Run all the tests to generate a solution 
     for j,word in enumerate(allowed):
-        # Make a copy of the raw word list
-        words = readFile(all_filename)
         # Frequecy data of words
         freq_list = readFileDict('./5-gram_freq.csv')
 
@@ -81,46 +81,68 @@ def simulate(stdscr:str, all_filename:str, allowed_filename:str, initial_guess:s
 
         # Get the guesses
         start = perf_counter()
+
+        # If the initial guess is to be hard coded
         guesses = wordleBot(words=words, 
                             freq_list=freq_list,
-                            arrangements=arramgements, 
+                            arrangements=arramgements,
+                            last_guess=initial_guess, 
                             simulate=True, 
                             simulate_data=simulate_data)
+        if initial_guess:
+            guesses.insert(0, initial_guess)
+
         end = perf_counter()
-        all_guesses.append((guesses, word in guesses and len(guesses)<7))
+        all_guesses.append({'guesses':guesses, 
+                            'moves':len(guesses), 
+                            'success':word in guesses and len(guesses)<7
+                        })
 
         # Get the moves made
         moves = [genFeedback(word, guess) for guess in guesses]
 
         # Guesses will be None if the bot failed to guess correctly
         stdscr.clear()
-        stdscr.addstr(0,0, f"{' Wordle Bot Simulator 🤖🏁'.center(90,'=')}")
+        stdscr.addstr(f"{' Wordle Bot Simulator 🤖🏁'.center(90,'=')}")
         stdscr.addstr(2,0, f"Word: {word}")
         stdscr.addstr(3,0, f"Guesses: {guesses}")
-        stdscr.addstr(4,60, f"Moves: {len(guesses)}")
-        stdscr.addstr(5,70, f"Solved: {word in guesses and len(guesses)<7}")
+        stdscr.addstr(4,0, f"Moves: {len(guesses)}")
+        stdscr.addstr(4,40, f"Solved: {word in guesses and len(guesses)<7}")
         stdscr.addstr(6,0, f"Tests run {j+1}")
         stdscr.addstr(6,30, f"Time taken: {round(end-start,4)} s")
-        printMoves(moves, stdscr, screen_row_offset=7)
+        printMoves(moves, stdscr, screen_row_offset=8)
         stdscr.refresh()
 
 
     return all_guesses
 
-wrapper(simulate, './wordle.txt', './allowed.txt')
 
-# if __name__=="__main__":
+if __name__=="__main__":
     
+    guesses = wrapper(simulate, './wordle.txt', './allowed.txt')
 
-    # # Calculate efficiency
-    # time_taken = end - start
-    # print("Time taken per word: ",time_taken)
+    # Sort the guesses by number of moves
+    guesses.sort(key=lambda x:x['moves'])
 
-    # # Success rate
-    # successes = list(filter(lambda x: x is not None, all_guesses))
-    # print(f"Success Rate: {100 * len(successes)/len(all_guesses)}%", )
+    # Success rate
+    successes = list(filter(lambda x:x['success'], guesses))
+    success_rate = len(successes)/len(guesses)
+    print(f"Success Rate: {100 * success_rate}%", )
 
-    # # Average number of guesses needed
-    # if successes:
-    #     guess_lengths = [len(guesses) for guesses in successes] 
-    #     print("Average guesses made: ", sum(guess_lengths)/len(successes))
+
+    # Number of moves
+    moves = []
+    i=0
+    for i in range(8):
+        moves.append(len(list(filter(lambda x:x['moves']==i+1, guesses))))
+    
+    print(f"{'Move Analysis'.center(50, '#')}")
+    print(f"Moves \t Frequency")
+    for j, move in enumerate(moves,1):
+        print(f"{j} \t {move}")
+
+    total_f, total_fx = 0,0
+    for i,move in enumerate(moves):
+        total_fx+=(i+1)*move
+        total_f+=move
+    print(f"Mean number of moves: {round(total_fx/total_f, 4)}")
